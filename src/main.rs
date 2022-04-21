3a00f1afdc343dcc280f1f4dede9f6fc586d37f8
theEndurance-del <tomarshubhanshu@protonmail.ch>
2022-04-21 15:55:23 +0530

    Use rustyline for line reader

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use rustyline::error::ReadlineError;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut reader = rustyline::Editor::<()>::new();
+        let in_string = match reader.readline("prompt> ") {
+            Ok(s) => s,
+            Err(e) => {
+                match e {
+                    ReadlineError::Eof => {
+                        break;
+                    }
+                    _ => {
+                        eprintln!("Err: {}", e);
+                        String::new()
+                    }
+                }
+            }
+        };
+        if in_string == "exit" {
+            break;
+        }
+    }
 }
