379fdfdd57650f28ca95db7431268a17fbad371e
stomar117 <tomarshubhanshu@protonmail.ch>
2022-04-27 20:49:28 +0530

    Proper indentation in `processor.rs`

--- src/utils/processor.rs
+++ src/utils/processor.rs
@@ -7,63 +7,62 @@ use splitters::{Splitters, Split};
 pub struct Process;
 
 impl Process {
-
-pub fn interactive () -> i32 {
-    let map = command_utils::init();
-    let mut reader = Editor::<()>::new();
-    loop {
-        let in_string = match reader.readline("> ") {
-            Ok(s) => {
-                reader.add_history_entry(&s);
-                println!("{}", reader.history().len());
-                s
-            },
-            Err(ReadlineError::Eof) => {return 0;},
-            Err(ReadlineError::Interrupted) => {return 130;},
-            Err(e) => {
-                println!("Error: {}", e);
-                String::new()
-            }
-        };
-        match Splitters::bracket(in_string.deref(), '(') {
-            Split::Split(vector) => {
-                println!("{:?}", vector);
-            },
-            Split::Failed(e) => {
-                println!("{}", e);
-            }
-            _ => {
-                println!("{}", in_string);
-            }
-        };
-        match Splitters::dbreaker(in_string.deref(), ' ') {
-            Split::Split(vector) => {
-                let mut arg_vec = vector.clone();
-                arg_vec.remove(0);
-                command_utils::run(vector[0].deref(), arg_vec, &map)
-            },
-            Split::Failed(e) => {
-                println!("{}", e);
-            }
-            _ => {
-                println!("1, {}", in_string);
-            }
-        };
-        match Splitters::quote(in_string.deref(), ' ') {
-            Split::Split(vector) => {
-                println!("{:?}", vector);
-            },
-            Split::Failed(e) => {
-                println!("{}", e);
-            }
-            _ => {
-                println!("1, {}", in_string);
-            }
-        };
-        // if in_string == "exit" {
-        //     break;
-        // } else {
+    pub fn interactive () -> i32 {
+        let map = command_utils::init();
+        let mut reader = Editor::<()>::new();
+        loop {
+            let in_string = match reader.readline("> ") {
+                Ok(s) => {
+                    reader.add_history_entry(&s);
+                    println!("{}", reader.history().len());
+                    s
+                },
+                Err(ReadlineError::Eof) => {return 0;},
+                Err(ReadlineError::Interrupted) => {return 130;},
+                Err(e) => {
+                    println!("Error: {}", e);
+                    String::new()
+                }
+            };
+            match Splitters::bracket(in_string.deref(), '(') {
+                Split::Split(vector) => {
+                    println!("{:?}", vector);
+                },
+                Split::Failed(e) => {
+                    println!("{}", e);
+                }
+                _ => {
+                    println!("{}", in_string);
+                }
+            };
+            match Splitters::dbreaker(in_string.deref(), ' ') {
+                Split::Split(vector) => {
+                    let mut arg_vec = vector.clone();
+                    arg_vec.remove(0);
+                    command_utils::run(vector[0].deref(), arg_vec, &map)
+                },
+                Split::Failed(e) => {
+                    println!("{}", e);
+                }
+                _ => {
+                    println!("1, {}", in_string);
+                }
+            };
+            match Splitters::quote(in_string.deref(), ' ') {
+                Split::Split(vector) => {
+                    println!("{:?}", vector);
+                },
+                Split::Failed(e) => {
+                    println!("{}", e);
+                }
+                _ => {
+                    println!("1, {}", in_string);
+                }
+            };
+            // if in_string == "exit" {
+            //     break;
+            // } else {
             // command_utils::run(in_string.deref(), &map);
+        }
     }
-}
 }
