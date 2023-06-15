464d3228a786250e0b14cc2befb3f61f447c4adc
Shubhanshu Tomar <tomarshubhanshu@protonmail.ch>
2023-06-15 20:25:16 +0530

    feat[nix]: update flake file and add lock file

--- flake.nix
+++ flake.nix
@@ -2,56 +2,50 @@
   description = "Rush a simple programmable shell written in rust";
 
   inputs = {
-    nixpkgs.url      = "github:nixos/nixpkgs/nixos-23.05";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-23.05";
+      follows = "rust-overlay/nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... } :
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ (import rust-overlay) ];
-          };
+  outputs = { nixpkgs,
+              rust-overlay,
+              flake-utils,
+              ...
+  } : flake-utils.lib.eachDefaultSystem (system : let
+      pkgconf = {
+        overlays = [ (import rust-overlay) ];
+        localSystem.system = system;
+      };
+      pkgs = import nixpkgs (pkgconf);
+    in {
+      packages.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "rush";
+        version = "0.1.0";
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
 
-          buildInputs = with pkgs;
-            [ openssl
-              pkg-config
-              rust-bin.stable.latest.default
-            ];
-      in {
-        devShells.default = pkgs.mkShell {
-          inherit buildInputs;
+        buildInputs = with pkgs;
+          [ openssl
+            pkg-config
+          ];
+      };
 
-          shellHook =
-            ''
-            alias build='cargo build'
-            alias run='cargo run'
-            alias test='cargo test'
-            '';
-        };
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs;
+          [ rust-bin.stable."1.70.0".default
+            pkg-config
+            openssl
+          ];
 
-        overlay = new : old : {
-          rush = with new; stdnev.mkDerivation {
-            inherit buildInputs;
-
-            pname = "rush";
-            src = self;
-
-            installPhase =
-              ''
-              mkdir -p $out/bin
-              cargo build --release
-
-              cp target/release/rush $out/bin/
-              '';
-          };
-        };
-
-        defaultPackage = nixpkgs.lib.genAttrs ["x86_64-linux"] (system: (import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays ];
-        }));
-      }
-  );
+        shellHook =
+          ''
+          alias build='cargo build'
+          alias run='cargo run'
+          alias test='cargo test'
+          '';
+      };
+    });
 }
